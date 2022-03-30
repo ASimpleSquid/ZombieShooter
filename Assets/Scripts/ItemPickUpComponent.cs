249ec38ded0a87201a898962f87fb33252ad671f
Yuki-Os <Traxiliongames@gmail.com>
2022-03-30 14:32:01 -0400

    Applied Weapon and Consumable pickups and imported Inventory menu, Finished Lab submission

--- Assets/Scripts/ItemPickUpComponent.cs
+++ Assets/Scripts/ItemPickUpComponent.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class ItemPickUpComponent : MonoBehaviour
 {
+    [SerializeField]
+    ItemScript pickupItem;
+
+    [Tooltip("Manual Override for drop amount, if left at -1 it will use the amount from scriptable object")]
+    [SerializeField]
+    int amount = -1;
+
+    [SerializeField] MeshRenderer propMeshRenderer;
+    [SerializeField] MeshFilter propMeshFilter;
+
+    ItemScript itemInstance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InstantiateItem();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InstantiateItem()
     {
-        
+        itemInstance = Instantiate(pickupItem);
+        if (amount > 0)
+        {
+            itemInstance.SetAmount(amount);
+        }
+        ApplyMesh();
+    }
+
+    void ApplyMesh()
+    {
+        if (propMeshFilter) propMeshFilter.mesh = pickupItem.itemPrefab.GetComponentInChildren<MeshFilter>().sharedMesh;
+        if (propMeshRenderer) propMeshRenderer.materials = pickupItem.itemPrefab.GetComponentInChildren<MeshRenderer>().sharedMaterials;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        //add to inventory here
+        //get reference to the player inventory, add item to it
+
+        Destroy(gameObject);
     }
 }
